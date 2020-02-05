25bcbd8bda189770d3d9b4997fc6f3165f65e5d8
Eirikur Jonsson <eirikur09@gmail.com>
2020-02-05 09:21:30 +0100

    nDescriptive function created

--- week1.py
+++ week1.py
@@ -11,6 +11,27 @@ except FileNotFoundError:
     df.to_csv("ETH_EUR", encoding = "utf-8")
     df2.to_csv("NASDAQOMX", encoding = "utf-8")
 
-print(df.head())
+# print(df.head())
+# print("=====================================================")
+# print(df2.head())
+
+def nDescriptive(df, year):
+    '''
+    Making a function that prints some descriptive numbers
+    for each column. 
+    TODO: I still havent gotten it to work for each year, meaning I want
+    to be able to select a specific year. 
+    '''
+    import itertools
+    nList = list(df.columns.values)
+    if year > 0:
+        for i in nList:
+            print("======================================================")
+            print(f"The Mean: {df[i].loc(df.index == year).mean()}")
+            print(f"The Median: {df[i].loc(df.index == year).median()}")
+            print(f"The Std: {df[i].loc(df.index == year).std()}")
+            print(f"The quantile: {df[i].loc(df.index == year).quantile([0.25,0.5,0.75])}")
+        
 
 
+nDescriptive(df, 2010)
